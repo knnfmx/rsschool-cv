f21a213a0b5d70e4d8d54761bb22838fcaeed256
kennyfmx <fmxhighlife@gmail.com>
2022-12-18 13:27:48 +0300

    feat: add change slides function, but all slider needs improvement

--- assets/scripts/slider-controls.js
+++ assets/scripts/slider-controls.js
@@ -3,13 +3,27 @@ console.log(projectContainer);
 let controlDot = document.querySelectorAll('.dot');
 console.log(controlDot);
 
+
+
 function changeSlide(ev) {
   controlDot.forEach(el => {
     el.classList.remove('active');
   });
   ev.target.classList.add('active');
 }
+     
 
-controlDot.forEach(el => {
-  el.addEventListener('click', changeSlide);
+
+controlDot[0].addEventListener('click', (el) => {
+  projectContainer.forEach(el => {
+    el.style.transform = "translateX(0px)";
+  });
+  changeSlide(el);
+});
+
+controlDot[1].addEventListener('click', (el) => {
+  projectContainer.forEach(el => {
+    el.style.transform = "translateX(-1000px)";
+  });
+  changeSlide(el);
 });
