2d783843e085363c8fcfc913265a8f8c01e4e147
kennyfmx <fmxhighlife@gmail.com>
2022-12-25 00:37:22 +0300

    feat: add header animation when menu btn is pressed

--- assets/scripts/burger.js
+++ assets/scripts/burger.js
@@ -2,15 +2,24 @@ const menuBtn = document.querySelector('#burger');
 const menu = document.querySelector('.menu__links-list');
 const menuItems = menu.querySelectorAll('.menu__links-list__item');
 const menuLinks = document.querySelectorAll('.menu__link');
+const menuBGShadow = document.querySelector('.menu-bg-shadow');
 
 function openMenu() {
   menuBtn.addEventListener('click', () => {
     if (menuBtn.classList.contains('_open')) {
       menuBtn.classList.remove('_open');
       menuBtn.classList.add('_close');
+      header.classList.remove('header_small');
+      menuBGShadow.classList.remove('menu-bg-shadow_visible');
+      menuBGShadow.classList.add('menu-bg-shadow_hidden');
+
+
     } else {
       menuBtn.classList.remove('_close');
       menuBtn.classList.add('_open');
+      header.classList.add('header_small');
+      menuBGShadow.classList.remove('menu-bg-shadow_hidden');
+      menuBGShadow.classList.add('menu-bg-shadow_visible');
     }
     
     menu.classList.toggle('_active');
@@ -28,6 +37,9 @@ function openMenu() {
         menuBtn.classList.add('_close');
         menuBtn.classList.remove('_open');
         menu.classList.remove('_active');
+        header.classList.remove('header_small');
+        menuBGShadow.classList.remove('menu-bg-shadow_visible');
+        menuBGShadow.classList.add('menu-bg-shadow_hidden');
         menuItems.forEach((link) => link.style.animation = '');
       });
     });
