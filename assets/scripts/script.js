8d831a42cb9eaf185a99e3a29306fb20672a1cec
kennyfmx <fmxhighlife@gmail.com>
2023-02-28 20:37:53 +0300

    feat: add phpmailer plugin to project

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -316,3 +316,73 @@ controlDots.forEach((dot, idx) => {
     activeSlide(sliderCount);
   });
 });
+
+// CONTACT FORM
+
+const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', formSend);
+
+async function formSend(e) {
+  e.preventDefault();
+  let error = formValidate(form);
+  let formData = new FormData(form);
+
+  if (error === 0) {
+    footer.classList.add('_sending');
+    body.style.overflow = 'hidden';
+    let response = await fetch('sendmail.php', {
+      method: 'POST',
+      body: formData
+    });
+    if (response.ok) {
+      let result = await response.json();
+      alert(result.message);
+      form.reset();
+      footer.classList.remove('_sending');
+      body.style.overflow = '';
+    } else {
+      alert('error');
+      footer.classList.remove('_sending');
+      body.style.overflow = '';
+    }
+  } else {
+    alert('Please required field');
+  }
+}
+
+function formValidate(form) {
+  let error = 0;
+  let formRequired = document.querySelectorAll('._req');
+  for (let i = 0; i < formRequired.length; i++) {
+    const input = formRequired[i];
+    formRemoveError(input);
+    if (input.classList.contains('_email')) {
+      if (emailTest(input)) {
+        formAddError(input);
+        error++;
+      } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+          formAddError(input);
+          error++;
+      } else {
+        if (input.value === '') {
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+  }
+  return error;
+}
+
+function formAddError(input) {
+  input.parentElement.classList.add('_error');
+  input.classList.add('_error');
+}
+function formRemoveError(input) {
+  input.parentElement.classList.remove('_error');
+  input.classList.remove('_error');
+}
+function emailTest(input) {
+  return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
